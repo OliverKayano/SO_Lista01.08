f2551664ffc74e2ddff5e3a0725fd9d414129b7b
OliverKayano <oliver12yoshio@hotmail.com>
2026-03-04 17:33:53 -0300

    Primeira versão

---
+++ SO_Lista01.08.py
@@ -0,0 +1,9 @@
+#Algoritmo SO_Lista01.08
+#Declarar.
+Depósito: float = 0;
+Patrimônio: float = 0;
+#Início.
+Depósito = float(input("Valor do depósito:"));
+Patrimônio = (Depósito*1.013);
+print(f"Valor após 1 mês: {Patrimônio:.2f}");
+#FIM.
